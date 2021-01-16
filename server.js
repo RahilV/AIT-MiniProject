d46d25d7962a70d13ba3ff6703ef1692190b5030
girishgr8 <girishthatte35@gmail.com>
2021-01-16 20:25:10 +0530

    register done

--- server.js
+++ server.js
@@ -2,13 +2,21 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
 const PORT = process.env.PORT || 3000;
+
+let database;
 
 // Serve static files (like css and js from public directory)
 app.use(express.static(__dirname + '/public/'));
+app.use(bodyParser.urlencoded({extended: false}));
 
 // views or templates path
 const views = __dirname + '/views/';
+
+// bcrypt rounds to salt
+var BCRYPT_SALT_ROUNDS = 12;
 
 // connect to mongodb database
 const MongoClient = require('mongodb').MongoClient;
@@ -16,8 +24,8 @@ const url = "mongodb+srv://ait:ait@cluster0.4bsqv.mongodb.net/ShreejiEstates?ret
 
 MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, (err, db) => {
 	if (err) throw err;
-	console.log("Database created!");
-	db.close();
+	database = db.db('ShreejiEstates');
+	// database.close();
 });
 
 // routes for navigation
@@ -39,6 +47,33 @@ app.get('/partners', (req, res) => {
 
 app.get('/buy', (req, res) => {
 	res.sendFile(path.join(views, '/maps.html'));
+});
+
+app.post('/login', (req, res) => {
+	const email = req.body.lemail;
+	const password = req.body.lpwd;
+
+	bcrypt.compare('somePassword', hash, (err, res) => {
+		if(res) {
+			// Passwords match
+		} else {
+			// Passwords don't match
+		} 
+	});
+});
+
+app.post('/register', (req, res) => {
+	const name = req.body.sname;
+	const email = req.body.semail;
+	const password = req.body.spwd;
+	const address = req.body.addr;
+	bcrypt.hash(password, BCRYPT_SALT_ROUNDS, (err, hash) => { 
+		const user = { name, email, hash, address };  
+		database.collection("users").insertOne(user, function(err, res) {  
+			if (err) throw err;  
+			console.log("New user registered !!"); 
+		});
+	});
 });
 
 // listen to the requests on given PORT
