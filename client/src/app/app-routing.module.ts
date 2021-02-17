06dfef8407370e496a20499dd6e34d6fc54b1421
girishgr8 <girishthatte35@gmail.com>
2021-02-17 17:44:19 +0530

    Angular setup done

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { SignupComponent } from './components/signup/signup.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'signup', component: SignupComponent },
+  { path: 'contactus', component: ContactUsComponent },
+  { path: '**', component: ErrorPageComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ client/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    SignupComponent,
+    ErrorPageComponent,
+    ContactUsComponent,
+    FooterComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
